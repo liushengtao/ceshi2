b30ccd330c3ff518e8bc179982bd23741bb6300d
youtu <youtu@yoututekiMacBook-Pro.local>
2015-03-10 16:21:02 +0800

    Initial Commit

---
+++ 练习2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  练习2
+//
+//  Created by youtu on 15/3/10.
+//  Copyright (c) 2015年 youtu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 练习2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  练习2
+//
+//  Created by youtu on 15/3/10.
+//  Copyright (c) 2015年 youtu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
